80cecf40db39acf05cf4f93a460b1bd69a714230
DIMASRZH\dimas <dimasrizkyhutama98@gmail.com>
2022-11-20 03:05:08 +0700

    Add Particles

--- Assets/Scripts/playerBehaviour.cs
+++ Assets/Scripts/playerBehaviour.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 [RequireComponent(typeof(SphereCollider))]
 public class playerBehaviour : MonoBehaviour
 {
-
+    [SerializeField] float healthPlayer;
+    
     void OnMouseDrag()
     {
         Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         point.z = gameObject.transform.position.z;
 
-        Debug.Log(point);
+        //Debug.Log(point);
 
-        if(point.y <= 9 && point.y >= 5 && point.x >= -1.5f && point.x <= 1.5f)
+        if(point.y <= 9 && point.y >= 5 && point.x >= -1.8f && point.x <= 1.8f)
         {
             gameObject.transform.position = point;
         }
@@ -24,6 +25,22 @@ public class playerBehaviour : MonoBehaviour
     void OnMouseUp()
     {
         Cursor.visible = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponentInChildren<ParticleSystem>())
+        {
+            
+            if(healthPlayer != 0)
+            {
+                healthPlayer--;
+            }
+            else if(healthPlayer == 0)
+            {
+                Debug.Log("GameOver");
+            }
+        }
     }
 }
 
