ba13d1750d22032813dda5f3fac49e57a11cd0f0
DIMASRZH\dimas <dimasrizkyhutama98@gmail.com>
2022-11-19 17:28:07 +0700

    puan added

--- Assets/Scripts/ballBehaviour.cs
+++ Assets/Scripts/ballBehaviour.cs
@@ -5,43 +5,55 @@ using UnityEngine;
 public class ballBehaviour : MonoBehaviour
 {
     [SerializeField] private float bounceCount;
-    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private Rigidbody2D _rigidbody;
     [SerializeField] private float totalATK;
     [SerializeField] private Vector3 ballForce;
     [SerializeField] private float increment;
+    private playerBehaviour playerBehaviour;
+    private float tes;
   
     void Start()
     {
-        _rigidbody = gameObject.AddComponent<Rigidbody>();
-        _rigidbody.AddForce(ballForce, ForceMode.Impulse);
-        _rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        _rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        _rigidbody.AddForce(ballForce, ForceMode2D.Impulse);
+       // _rigidbody.gravityScale = 0;
         
 
     }
-
-      void Update()
+    private void FixedUpdate()
     {
-        
+        tes = Mathf.SmoothStep(50, 100, 0 / 60);
+    }
+
+    void Update()
+    {
+
         //ballForce = _rigidbody.velocity;
         //_rigidbody.velocity = ballForce * increment;
-        ballForce = _rigidbody.velocity;
-        _rigidbody.velocity =  ballForce.normalized * (increment + 3);
+        //ballForce = _rigidbody.velocity;
        
 
     }
 
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "wall")
         {
             increment++;
             bounceCount++;
-            totalATK += bounceCount+5;
+            totalATK += bounceCount;
+            //Debug.Log(totalATK += bounceCount + _rigidbody.velocity.magnitude);
+            Debug.Log(_rigidbody.velocity.magnitude);
+        } else if(collision.gameObject.tag == "Player"){
+            //ballForce = _rigidbody.velocity;
+            //_rigidbody.velocity = ballForce * ;
+            _rigidbody.AddForce(ballForce, ForceMode2D.Impulse);
             Debug.Log(_rigidbody.velocity.magnitude);
         }
     }
-
+   
    
         
     
---
+++ Assets/playerBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SphereCollider))]
+public class playerBehaviour : MonoBehaviour
+{
+    void OnMouseDrag()
+    {
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        point.z = gameObject.transform.position.z;
+        gameObject.transform.position = point;
+        Cursor.visible = false;
+    }
+
+    void OnMouseUp()
+    {
+        Cursor.visible = true;
+    }
+}
+
