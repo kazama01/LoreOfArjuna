48fe8f5ae2e7da9f6c147d9cfa5b37f668436638
DIMASRZH\dimas <dimasrizkyhutama98@gmail.com>
2022-11-19 14:05:12 +0700

    gameJam

---
+++ Assets/Scripts/ballBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballBehaviour : MonoBehaviour
+{
+    [SerializeField] private float bounceCount;
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float totalATK;
+    [SerializeField] private Vector3 ballForce;
+    [SerializeField] private float increment;
+  
+    void Start()
+    {
+        _rigidbody = gameObject.AddComponent<Rigidbody>();
+        _rigidbody.AddForce(ballForce, ForceMode.Impulse);
+        _rigidbody.constraints = RigidbodyConstraints.FreezePositionY;
+        
+
+    }
+
+      void Update()
+    {
+        
+        //ballForce = _rigidbody.velocity;
+        //_rigidbody.velocity = ballForce * increment;
+        ballForce = _rigidbody.velocity;
+        _rigidbody.velocity =  ballForce.normalized * (increment + 3);
+       
+
+    }
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "wall")
+        {
+            increment++;
+            bounceCount++;
+            totalATK += bounceCount+5;
+            Debug.Log(_rigidbody.velocity.magnitude);
+        }
+    }
+
+   
+        
+    
+}
